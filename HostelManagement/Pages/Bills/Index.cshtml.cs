066825feaf35741a4b29fb382ccef2a0cdc2d4a6
datlt2001 <thanhdat3001@gmail.com>
2022-07-21 00:03:21 +0700

    Bills for Owner

--- HostelManagement/Pages/Bills/Index.cshtml.cs
+++ HostelManagement/Pages/Bills/Index.cshtml.cs
@@ -10,6 +10,8 @@ using DataAccess;
 using DataAccess.Repository;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using HostelManagement.Helpers;
+using Microsoft.AspNetCore.Http;
 
 namespace HostelManagement.Pages.Bills
 {
@@ -17,18 +19,23 @@ namespace HostelManagement.Pages.Bills
     public class IndexModel : PageModel
     {
         private IBillRepository billRepository { get; }
-        public IndexModel(IBillRepository _billRepository)
+        private ISendMailService sendMailService { get; }
+        public IndexModel(IBillRepository _billRepository, ISendMailService _sendMailService)
         {
             billRepository = _billRepository;
+            sendMailService = _sendMailService;
         }
 
 
         public IEnumerable<Bill> Bill { get;set; }
 
-        public async Task<IActionResult> OnGetAsync(int id)
+        public async Task<IActionResult> OnGetAsync()
         {
-            Bill = await billRepository.GetBillList();
-            Bill = Bill.Where(bill => bill.RentId == id);
+            if (!String.IsNullOrEmpty(HttpContext.Session.GetString("sendmail")))
+            {
+                ViewData["mailMessage"] = "Send mail successfully";
+                HttpContext.Session.Remove("sendmail");
+            }
             int UId = 0;
             var userId = HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
             if (userId != null)
@@ -39,6 +46,11 @@ namespace HostelManagement.Pages.Bills
             {
                 return RedirectToPage("/AccessDenied");
             }
+            Bill = await billRepository.GetBillList();
+            Bill = Bill
+                .Where(bill => bill.Rent.Room.Hostel.HostelOwnerEmailNavigation.UserId == UId)
+                .Where(bill => (bill.DueDate != bill.CreatedDate && bill.DueDate < DateTime.Now));
+            
             var userRole = HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value;
             if (userRole == null)
             {
@@ -49,6 +61,22 @@ namespace HostelManagement.Pages.Bills
                 Bill.OrderByDescending(r => r.CreatedDate);
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnGetSendMailAsync(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            string body = "Dear, \n" +
+                    "You delay your bill. Please pay bill soon. (" +
+                    "). Thank you. \n" +
+                    "Please send your feedback by reply mail.\n" +
+                    "Best Regard,";
+            await sendMailService.SendEmailAsync(id, "Remind pay bill", body);
+            HttpContext.Session.SetString("sendmail", "Send mail successfully");
+            return RedirectToPage("./Index");
         }
     }
 }
